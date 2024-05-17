dc249c1e064c6a6f184f40f3070a596d0fb46790
amjadmehmoodnoborderz <amjad@noborderz.com>
2024-05-17 12:33:52 +0500

    cors

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const corsOptions ={
 }
 // Set up middleware
 app.use(cors({
-  origin: 'https://zubair-marriage-center.netlify.app/', // use your actual domain name (or localhost), using * is not recommended
+  origin: 'https://zubair-marriage-center.netlify.app', // use your actual domain name (or localhost), using * is not recommended
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
   credentials: true
--- frontend/src/ServerRequests/GlobalSender.js
+++ frontend/src/ServerRequests/GlobalSender.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 const API=axios.create({
-    baseURL:process.env.REACT_APP_BASE_URI
+    baseURL:process.env.REACT_APP_BASE_URI,
+    withCredentials: true,
 })
 
 export default API
