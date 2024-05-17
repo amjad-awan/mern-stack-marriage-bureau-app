f025cb87c7d9d224034411d358fc459145e28297
amjadmehmoodnoborderz <amjad@noborderz.com>
2024-05-17 16:47:26 +0500

    server timeout fixed

--- backend/db.js
+++ backend/db.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect("mongodb+srv://amjadmalikf53:hFyCiAaO5rKStR6d@cluster0.tbisklz.mongodb.net/marriage-app-fsd?retryWrites=true&w=majority&appName=Cluster0",{
       useNewUrlParser: true, // Use the new URL parser
       useUnifiedTopology: true, // Use the new Unified Topology engine
+      serverSelectionTimeoutMS: 50000,
     });
     console.log(
       `data base is connected to host ${conn.connection.host}`
