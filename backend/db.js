58d17f6990802a0a9cbdc056dca58e4011f064c7
amjadmehmoodnoborderz <amjad@noborderz.com>
2024-05-17 11:51:13 +0500

    api issue fixed

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,22 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://amjadmalikf53:hFyCiAaO5rKStR6d@cluster0.tbisklz.mongodb.net/marriage-app-fsd?retryWrites=true&w=majority&appName=Cluster0");
-
-    console.log(
-      `data base is connected to host ${conn.connection.host}`
+    const conn = await mongoose.connect(
+      "mongodb+srv://amjadmalikf53:hFyCiAaO5rKStR6d@cluster0.tbisklz.mongodb.net/marriage-app-fsd?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
     );
+
+    console.log(`Database connected to host: ${conn.connection.host}`);
   } catch (error) {
-    console.log(`Error in db ${error}`);
+    console.log(`Error in DB connection: ${error.message}`);
+    process.exit(1); // Exit process with failure
   }
 };
 
